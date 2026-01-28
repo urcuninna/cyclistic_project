8302ae9f1c25ae55e678cb6a5a93b1b032e4046d
Erika Yesid <erika00yesid@gmail.com>
2026-01-27 22:12:11 -0500

    Se creó el archivo eda_diagnostico, por ahora solo se ha hecho un análisis exploratorio general: forma, tipo de datos, nulos, valores únicos

--- integracion.py
+++ integracion.py
@@ -10,7 +10,7 @@ ruta = glob.glob(
     r"C:\Users\ERIKA PINCHAO\OneDrive\Documentos\Data science\cyclistic_project\*.csv")
 lista_df = [pd.read_csv(archivo) for archivo in ruta]
 df_maestro = pd.concat(lista_df, ignore_index=True)
-df_maestro.to_csv(r"C:\Users\ERIKA PINCHAO\OneDrive\Documentos\Data science\cyclistic_project\dataset_maestro_ciclistas.csv", index=False)
+df_maestro.to_csv(
+    r"C:\Users\ERIKA PINCHAO\OneDrive\Documentos\Data science\cyclistic_project\dataset_maestro_ciclistas.csv", index=False)
 
 print("¡Archivo exportado con éxito!")
-
